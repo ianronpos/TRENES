bcdcb25bbc74148730c27cbd639867632a37595d
ian ronco postigo <ian.ronco2006@gmail.com>
2025-03-13 16:36:23 +0100

    Clase lab

---
+++ src/fp/trenes/TrayectoTrenImpl.java
@@ -0,0 +1,99 @@
+package fp.trenes;
+
+import java.time.Duration;
+import java.time.LocalTime;
+import java.util.List;
+
+public class TrayectoTrenImpl implements TrayectoTren {
+
+	private String codigoTren; 
+	private String nombre; 
+	private TipoTren tipoTren; 
+	private List<String> estaciones; 
+	private List<LocalTime> horaLlegada; 
+	private List<LocalTime> horaSalida; 
+	@Override
+	public String getCodigoTren() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getNombre() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public TipoTren getTipoTren() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<String> getEstaciones() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<LocalTime> getHorasLLegada() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public LocalTime getHoraSalida() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public LocalTime getHoraLlegada() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public LocalTime getHoraSalida(String estacion) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public LocalTime getHoraLlegada(String estacion) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Duration getDuracionTrayecto() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void anadirEstacionIntermedia(int posicion, String estacion, LocalTime horaLlegada, LocalTime horaSalida) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void eliminarEstacionIntermedia(String estacion) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int compareTo(TrayectoTren o) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public List<LocalTime> getHorasSalida() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
